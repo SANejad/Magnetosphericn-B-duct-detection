1e6a2ee89aed45135ab649f6cabb1d33e6723248
Sal <sal.adkins84@gmail.com>
2026-03-17 14:20:51 -0400

    Update __init__.py
    Add event dataset construction and duct quicklook plotting modules

--- src/duct_detection/__init__.py
+++ src/duct_detection/__init__.py
@@ -9,3 +9,38 @@ from .data_ingest import (
     save_interval_csv,
     save_interval_netcdf,
 )
+
+from .event_dataset import (
+    EventConfig,
+    build_event_dataset,
+    export_event_datasets_from_csv,
+    save_event_dataset,
+)
+
+from .plotting import (
+    batch_plot_event_quicklooks,
+    plot_bt_epsd,
+)
+
+__all__ = [
+    # data_ingest
+    "MMSInterval",
+    "build_interval_dataframe",
+    "build_interval_dataset",
+    "convert_to_datetime",
+    "interpolate_to_common_time",
+    "load_intervals_from_csv",
+    "load_mms_core_data",
+    "save_interval_csv",
+    "save_interval_netcdf",
+
+    # event_dataset
+    "EventConfig",
+    "build_event_dataset",
+    "export_event_datasets_from_csv",
+    "save_event_dataset",
+
+    # plotting
+    "batch_plot_event_quicklooks",
+    "plot_bt_epsd",
+]
